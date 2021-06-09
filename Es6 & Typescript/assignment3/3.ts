b45323ae69dd370a4349d6407bb856048a690696
swaroop lute <84199550+swaroop-git@users.noreply.github.com>
2021-06-09 19:46:38 +0530

    Add files via upload

---
+++ Es6 & Typescript/assignment3/3.ts
@@ -0,0 +1,19 @@
+//1)promises
+/*let x = 0;
+let  y = 0;
+
+function add(x,y) {
+    return new Promise((resolve,reject) => {
+        if (x+y){
+            resolve(x+y)
+        }
+        else{
+            reject("Could not add two values!!!")
+        }
+    })
+}
+
+add(x,y).then( (message) => {console.log(message)}).catch( (message) => {console.log(message)}) */
+
+
+//2)
