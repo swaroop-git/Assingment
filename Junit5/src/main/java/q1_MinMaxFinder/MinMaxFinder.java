6913dc9c2a001cf476434a578a3754f1d9f26be1
swaroop lute <84199550+swaroop-git@users.noreply.github.com>
2021-10-29 11:24:00 +0530

    Add files via upload

---
+++ Junit5/src/main/java/q1_MinMaxFinder/MinMaxFinder.java
@@ -0,0 +1,34 @@
+package q1_MinMaxFinder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MinMaxFinder {
+
+	private int min, max;
+	
+	public MinMaxFinder() {}
+	
+	public MinMaxFinder(int min, int max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	public List<Integer> findMinMax(List<Integer> inputList){
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(Collections.min(inputList));
+		list.add(Collections.max(inputList));
+	
+		return list;
+	}
+
+	public int getMin() {
+		return min;
+	}
+
+	public int getMax() {
+		return max;
+	}
+
+}
---
+++ Junit5/src/main/java/q4_Hooks/HooksArith.java
@@ -0,0 +1,8 @@
+package q4_Hooks;
+
+public class HooksArith {
+	
+	public int add(int a, int b) {
+		return a+b;
+	}
+}
